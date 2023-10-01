5acbd709a2353593b6975a6d76510eaf7ac424ca
warben <quirox01@gmail.com>
2023-10-01 16:48:57 -0300

    update ruling searcher

--- src/components/RulingForm.tsx
+++ src/components/RulingForm.tsx
@@ -1,16 +1,50 @@
 import { useState } from "react";
 import SearchInput from "./SearchInput/SearchInput.jsx";
+import { Input } from "./ui/input.js";
 
-export default function RulingForm(data: any) {
+export default function RulingForm({ data }: any) {
   const [card, setCard] = useState({} as any);
-  const handleSelection = (card: any) => {
-    console.log(card);
-    setCard(card);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e: any) => {
+    const { value } = e.target;
+    setInputValue(value);
+    const cardData = data.find(
+      (e: any) => e.card_name + " " + e.card_number === value
+    );
+    if (cardData) setCard(cardData);
   };
   return (
     <>
-      <SearchInput handleSelection={handleSelection} data={data.data} />
-      <article>{card?.card_name}</article>
+      <div className="w-[100%] flex gap-4 items-center justify-center mb-5 ">
+        <div className="flex flex-col w-[70%]">
+          <Input
+            placeholder="Search a card..."
+            type="text"
+            list="data"
+            onChange={handleInput}
+            value={inputValue}
+          />
+        </div>
+      </div>
+      {card?.card_name ? (
+        <article className="flex flex-col items-center justify-center">
+          <label>
+            {card?.card_name} ({card?.card_number})
+          </label>
+          <img className="w-[30%]" src={card?.image_url}></img>
+        </article>
+      ) : (
+        ""
+      )}
+      <datalist id="data">
+        {data.map((e: any, i: number) => (
+          <option
+            key={e.card_number + "*" + i}
+            value={e.card_name + " " + e.card_number}
+          />
+        ))}
+      </datalist>
     </>
   );
 }
