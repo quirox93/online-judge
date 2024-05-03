630d4b6c66d9dba1cff0b2a0388da01bdbbf1a62
quirox93 <quirox01@gmail.com>
2024-05-03 19:11:08 -0300

    update 10 files update prices.ts

--- src/pages/api/prices.ts
+++ src/pages/api/prices.ts
@@ -4,5 +4,9 @@ export const POST = async ({ request }: any) => {
   const { card_number, card_name } = await request.json();
   const data = await getPriceData(card_number, card_name);
   console.log(data);
-  return new Response(JSON.stringify(data));
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
 };
